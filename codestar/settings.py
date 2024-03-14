480c96497b7969691000484aa3ca971af1082eda
swathi keshavamurthy <swathik.2197@gmail.com>
2024-03-14 00:44:31 +0000

    Add a POST model

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#anq9a0w7ijxu#@*5=y-qs!(q%sw5fe9fdahzs#h#(0=+t^u14'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -91,6 +92,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
