9a153ed61221c8a045b983ebb91c50d3d0eb409c
swathi keshavamurthy <swathik.2197@gmail.com>
2024-03-15 21:55:20 +0000

    Building the homepage with Django generic views

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-swathikeshav-djangoblog-0fhd118cqjr.ws-eu110.gitpod.io',
+ALLOWED_HOSTS = ['8000-swathikeshav-djangoblog-fjib4ypym0u.ws-eu110.gitpod.io',
                 '.herokuapp.com']
 
 
