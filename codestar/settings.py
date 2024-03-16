50c62ae38539e7264d91bb6df4e7bbe2180b01fb
swathi keshavamurthy <swathik.2197@gmail.com>
2024-03-16 00:26:47 +0000

    Add static files

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-swathikeshav-djangoblog-fjib4ypym0u.ws-eu110.gitpod.io',
                 '.herokuapp.com']
